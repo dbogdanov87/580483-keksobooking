5629bd15a394b1f7b9e9c781b60ed9724f5175f0
dbogdanov <dbogdanov87@yandex.ru>
2018-05-07 15:24:57 +0300

    Вынес поиск img в переменную elementPinImg

--- js/pin.js
+++ js/pin.js
@@ -9,13 +9,14 @@
   window.pin = {
     createPin: function (object, index) {
       var elementPin = mainPin.cloneNode(true);
+      var elementPinImg = elementPin.querySelector('img');
       elementPin.querySelector('svg').remove();
       elementPin.classList.remove('map__pin--main');
       elementPin.classList.add('pin');
       elementPin.style.left = object.location.x + 'px';
       elementPin.style.top = object.location.y + 'px';
-      elementPin.querySelector('img').src = object.author.avatar;
-      elementPin.querySelector('img').alt = object.offer.title;
+      elementPinImg.src = object.author.avatar;
+      elementPinImg.alt = object.offer.title;
       elementPin.setAttribute('value', index);
       return elementPin;
     },
